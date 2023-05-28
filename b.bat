1304184e2180c6cca4f7d2b13bcac2e7424e5822
Vladimir1762 <84678039+Vladimir1762@users.noreply.github.com>
2023-05-28 12:13:50 +0300

    ййй

--- b.bat
+++
@@ -1,7 +0,0 @@
-echo "# mWaiter" >> README.md
-git init
-;;git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Vladimir1762/mWaiter.git
-git push -u origin main
--- d.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "fix bug"
-git push
